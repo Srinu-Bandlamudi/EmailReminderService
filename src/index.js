0a6d7f50b25259dbc9c084c45f91d44e75c98273
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-14 15:57:15 +0530

    Installed node-cron package

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 
-const { sendBasicEmail }=require('./services/email-service');
+//const { sendBasicEmail }=require('./services/email-service');
+
+const cron=require('node-cron');
 
 const { PORT }=require('./config/serverConfig');
 
@@ -12,13 +14,15 @@ const setUpAndStartServer=()=>{
 
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
-        sendBasicEmail(
-            'support@gmail.com',
-            'srinubandlamudi55@gmail.com',
-            'This is a testing email',
-            'Hey,How are you ,This is my first email'
-        );
-
+        // sendBasicEmail(
+        //     'support@gmail.com',
+        //     'srinubandlamudi55@gmail.com',
+        //     'This is a testing email',
+        //     'Hey,How are you ,This is my first email'
+        // );
+            cron.schedule('*/2 * * * *',()=>{
+                    console.log("Cron job for every two minutes");
+            });
     });
 
 }
